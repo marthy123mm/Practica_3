eba4d2a896ba21b946fa65092de35f94eb0092e6
marthy123mm <loscuisillos@hotmail.com>
2015-03-10 22:20:56 -0600

    Se agrego el constructor de la clase y un metodo para agregar Alumnos

--- Program.cs
+++ Program.cs
@@ -1,25 +1,29 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Martin
- * Date: 10/03/2015
- * Time: 09:33 p. m.
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+﻿
 using System;
 
 namespace Practica_3
 {
 	class Program
 	{
+		private Hashtable tabla;
+		
+		public Program(){
+			tabla=new Hashtable();	
+		}
+		public void agregarAlumno(){
+			String nombre;
+			String codigo;
+			Console.Clear();
+			Console.Write("Nombre del alumno: ");
+			nombre=Console.ReadLine();
+			Console.Write("Codigo del alumno: ");
+			codigo=Console.ReadLine();
+			Alumno a=new Alumno(nombre,codigo);
+			tabla.Add(codigo,a);
+			Console.Clear();
+		}
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
 		}
 	}
 }
