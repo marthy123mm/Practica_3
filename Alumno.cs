f3da93bdbfec9d9d33c42c548ea6c38e5d192707
marthy123mm <loscuisillos@hotmail.com>
2015-03-10 22:14:21 -0600

    Se agrego la clase Alumno con sus metodos ya incluidos

---
+++ Alumno.cs
@@ -0,0 +1,39 @@
+﻿//CLASE ALUMNO
+using System;
+
+namespace Practica_3
+{
+
+	public class Alumno
+	{
+		private String nombre;
+		private String codigo;
+		
+		public Alumno(String n, String c)
+		{
+			nombre=n;
+			codigo=c;
+		}
+		public Alumno(){
+			nombre="";
+			codigo="";
+		}
+		
+		public void setNomnre(String nombre){
+			this.nombre=nombre;
+		}
+		public String getNombre(){
+			return nombre;
+		}
+		public void setCodigo(String nombre){
+			this.nombre=nombre;
+		}
+		public String getCodigo(){
+			return codigo;
+		}
+		
+		public String getDatos(){
+			return "nombre: "+nombre+"\n" + "Codigo: "+codigo;
+		}
+	}
+}
